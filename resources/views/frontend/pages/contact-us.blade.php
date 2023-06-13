72f86dd71688850536e81499aeb6913eac2a53bd
dhameliyasanjay <dhameliyasanjay1995@gmail.com>
2023-06-13 17:49:39 +0530

    meta add

--- resources/views/frontend/pages/contact-us.blade.php
+++ resources/views/frontend/pages/contact-us.blade.php
@@ -84,8 +84,8 @@
                 </div>
             </div>
             <!-- Map Section -->
-            <div class="row">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.274196716918!2d72.82055951744384!3d21.18126380000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x409794731bd7aacb%3A0xf376382e55bcceb2!2sVIBRAMADE!5e0!3m2!1sen!2sin!4v1685517261000!5m2!1sen!2sin" width="100%" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="row map_size">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.274196716918!2d72.82055951744384!3d21.18126380000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x409794731bd7aacb%3A0xf376382e55bcceb2!2sVIBRAMADE!5e0!3m2!1sen!2sin!4v1685517261000!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </section>
